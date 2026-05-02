9541bb1c2a081129e7c4fe88c9d5fc25bee99112
chamudiwalakanda <chamudiwalakanda@gmail.com>
2026-05-02 19:30:27 +0530

    Fetch products from DummyJSON API

---
+++ src/pages/Home.vue
@@ -0,0 +1,12 @@
+import { ref, onMounted } from 'vue'
+import type { Product } from '../types/product'
+
+const products = ref<Product[]>([])
+const loading = ref(true)
+
+onMounted(async () => {
+  const res = await fetch('[dummyjson.com](https://dummyjson.com/products)')
+  const data = await res.json()
+  products.value = data.products
+  loading.value = false
+})
